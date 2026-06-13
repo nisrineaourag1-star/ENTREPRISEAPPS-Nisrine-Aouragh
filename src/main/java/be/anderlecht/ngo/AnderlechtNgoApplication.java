78579db50d7d2a1adfbd0126ddaf5a8eec951421
nisrineaourag1-star <nisrineaourag1@gmail.com>
2026-06-13 16:52:27 +0200

    Projectstructuur opgezet en pom.xml geconfigureerd

---
+++ src/main/java/be/anderlecht/ngo/AnderlechtNgoApplication.java
@@ -0,0 +1,12 @@
+package be.anderlecht.ngo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnderlechtNgoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnderlechtNgoApplication.class, args);
+    }
+}
